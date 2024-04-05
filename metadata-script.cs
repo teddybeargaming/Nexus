b37efcdd6dca46420a6b4c7719fe6d5f3f98bbc5
teddybeargaming <160274508+teddybeargaming@users.noreply.github.com>
2024-04-05 20:45:20 +0100

    Metadata script (needs to be a C# script)

---
+++ metadata-script.cs
@@ -0,0 +1,6 @@
+using SystemCollections;
+using Nexus3D;
+//Loads "locales" folder, which contains game's metadata
+#include "locales/"
+  //Needs screen refresh, which can only be called in a C# script
+  void Update();
