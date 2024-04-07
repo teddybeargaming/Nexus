5e04ae17022b1ba152dbf7d1bc86c42034934f1e
teddybeargaming <160274508+teddybeargaming@users.noreply.github.com>
2024-04-07 19:51:41 +0100

    Community SDK base

---
+++ CommunitySDKBase.c
@@ -0,0 +1,7 @@
+using SystemCollections;
+using Nexus3D;
+//Code to class this as community SDK, and run mod executable
+SDKClass = "community"
+  if SDKClass = "community"
+nexium.IgnoreFile "GameExec.exe"
+  nexium.GameApp "ModLoader.bat"
