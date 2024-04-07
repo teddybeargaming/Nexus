09805699a28f3474640d86e8998f40afb21efbd9
teddybeargaming <160274508+teddybeargaming@users.noreply.github.com>
2024-04-07 13:14:40 +0100

    Graphics script

---
+++ GraphicScript.c
@@ -0,0 +1,8 @@
+SystemCollections;
+Nexus3D;
+//"screen_res" variable has current screen resolution
+screen_res = 1080
+  //You can change the "screen_res" var to your resolution
+  console.WriteLine"Current screen resolution= (screen_res)"
+  #EndLocal
+  
